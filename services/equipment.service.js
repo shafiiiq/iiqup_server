3eb2da4f7fce42cc9dfe2ffa2b8d463d21760dee
Muhammed Shafeek <shafiiq688@gmail.com>
2026-05-10 15:06:55 +0300

    fix: operations emailing bug

--- services/equipment.service.js
+++ services/equipment.service.js
@@ -936,15 +936,16 @@ const demobilizeEquipment = async (data) => {
     });
 
     await alertMobilizationViaEmail({
-      action:           'demobilized', regNo, machine,
+      action:             'demobilized', regNo, machine,
       month, year, time, date: selectedDate ? new Date(selectedDate) : new Date(), remarks,
-      site:             Array.isArray(currentSite) ? currentSite.at(-1) || '' : currentSite || '',
-      hired:            currentEquipment?.hired     || false,
-      hiredFrom:        currentEquipment?.hiredFrom || '',
-      rentRate:         currentEquipment?.rentRate  || null,
-      location:         currentEquipment?.location  ? [currentEquipment.location] : [],
-      operator:         currentOperatorName,
-      withOperator:     !!currentOperatorName,
+      site:               Array.isArray(currentSite) ? currentSite.at(-1) || '' : currentSite || '',
+      hired:              currentEquipment?.hired     || false,
+      hiredFrom:          currentEquipment?.hiredFrom || '',
+      rentRate:           currentEquipment?.rentRate  || null,
+      location:           currentEquipment?.location  ? [currentEquipment.location] : [],
+      operator:           currentOperatorName,
+      withOperator:       !!currentOperatorName,
+      previousOperators:  currentEquipment?.certificationBody || [],
       lastMobilizedDate,
       lastMobilizedTime,
     }).catch(e => console.error('Demobilization email failed:', e));
